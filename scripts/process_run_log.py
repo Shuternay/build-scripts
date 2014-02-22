9227f2ec26adb8373cc1dbb9e51759e6fdfcf0a8
shuternay <kiselev.sg@gmail.com>
2014-02-22 23:58:39 +0700

    improved run log processing

--- scripts/process_run_log.py
+++ scripts/process_run_log.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3.3
+"""
+Prepare internal ejudge xml runlog to import runs in another contest
+This script converts names of problems and moves time of runs to another reference frame
+"""
 import sys
 
 __author__ = 'ksg'
@@ -19,13 +23,18 @@ cur_start_time = time.mktime(time.strptime(runlog_root.attrib['start_time'], '%Y
 archive_start_time = time.mktime(time.strptime('2013/10/07 23:08:14', '%Y/%m/%d %H:%M:%S'))
 time_offset = cur_start_time - archive_start_time
 
-prob_pref = 'omsu13-14-r1_'
+if len(sys.argv) > 2:
+    prob_prefix = sys.argv[2]
+else:
+    prob_prefix = input('problem name prefix: ')
+
+convert_prob_name = lambda x: prob_prefix + x
 
 for run in runlog_root[0]:
     run_time = int(run.attrib['time']) + int(time_offset)
     run.set('time', str(run_time))
 
-    run_prob_id = prob_pref + run.attrib['prob_short']
-    run.set('prob_short', run_prob_id)
+    run_prob_name = convert_prob_name(run.attrib['prob_short'])
+    run.set('prob_short', run_prob_name)
 
 runlog.write('new_' + path)
